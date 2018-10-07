0a53bc5c9fc40e84a5f0e0bef5cafd60fd8534c5
Kalle <qalle85@gmail.com>
2018-10-07 05:32:32 +0300

    move help to readme

--- ines_split.py
+++ ines_split.py
@@ -3,47 +3,24 @@ import os.path
 import struct
 import sys
 
-HELP_TEXT = """\
-Copies Trainer, PRG-ROM or CHR-ROM from an iNES ROM file (.nes) to a
-new file.
-
-Command line arguments: OUTPUT_FILES INPUT_FILE
-    OUTPUT_FILES:
-        Which parts to read from INPUT_FILE and names of files to write
-        (one or more):
-            -t OUTPUT_FILE, --trainer=OUTPUT_FILE
-                Trainer (512 bytes)
-            -p OUTPUT_FILE, --prg-rom=OUTPUT_FILE
-                PRG-ROM
-            -c OUTPUT_FILE, --chr-rom=OUTPUT_FILE
-                CHR-ROM
-        The files must not already exist (they will not be overwritten).
-    INPUT_FILE:
-        iNES ROM file to read.
-        This argument is required.
-
-Note: If the PRG-ROM or the CHR-ROM consists of a repeated chunk with
-a length of a power of two, only one instance will be extracted. For
-example, the Game Genie iNES file has 16 KiB of PRG-ROM but only 4 KiB
-will be extracted because the rest is just the same data repeated.\
-"""
-
 # maximum size of buffer when reading files, in bytes
 FILE_BUFFER_MAX_SIZE = 2 ** 20
 
 # iNES file part sizes
 HEADER_SIZE = 16
-TRAINER_SIZE = 512  # if present
+TRAINER_SIZE = 512
 PRG_BANK_SIZE = 16 * 1024
 CHR_BANK_SIZE = 8 * 1024
 
 def to_ASCII(string):
     """Replace non-ASCII characters with backslash codes."""
+
     byteString = string.encode("ascii", errors="backslashreplace")
     return byteString.decode("ascii")
 
 def validate_output_file(file):
     """Exit if output file is invalid."""
+
     if os.path.exists(file):
         exit("Error: file already exists: {:s}".format(to_ASCII(file)))
     dir = os.path.dirname(file)
@@ -51,12 +28,17 @@ def validate_output_file(file):
         exit("Error: directory does not exist: {:s}".format(to_ASCII(dir)))
 
 def parse_arguments():
+    """Parse command line arguments using getopt."""
+
+    shortOpts = "t:p:c:"
+    longOpts = ("trainer=", "prg-rom=", "chr-rom=")
     try:
-        (opts, args) = getopt.getopt(
-            sys.argv[1:], "t:p:c:", ("trainer=", "prg-rom=", "chr-rom=")
-        )
+        (opts, args) = getopt.getopt(sys.argv[1:], shortOpts, longOpts)
     except getopt.GetoptError:
-        exit("Error: invalid argument")
+        exit("Error: invalid argument. See the readme file.")
+
+    if len(args) != 1:
+        exit("Error: invalid number of arguments. See the readme file.")
 
     opts = dict(opts)
 
@@ -75,16 +57,14 @@ def parse_arguments():
     if chr is not None:
         validate_output_file(chr)
 
-    # at least one target file is required
+    # must have at least one output file
     if trainer is None and prg is None and chr is None:
-        exit("Error: nothing to do")
+        exit("Error: at least one output file argument is required.")
 
     # input file
-    if len(args) != 1:
-        exit("Error: invalid number of arguments")
     source = args[0]
     if not os.path.isfile(source):
-        exit("Error: file not found: {:s}".format(to_ASCII(source)))
+        exit("Error: file not found.")
 
     return {
         "trainer": trainer,
@@ -95,6 +75,7 @@ def parse_arguments():
 
 def parse_iNES_header(header, file):
     """Parse an iNES header."""
+
     # extract fields
     fields = struct.unpack("4s4B8s", header)
     (id, prgBanks, chrBanks, flags6, flags7, reservedBytes) = fields
@@ -108,7 +89,7 @@ def parse_iNES_header(header, file):
         print("Warning: reserved header bits are nonzero: {:s}".format(
             to_ASCII(file)
         ))
-    # return sizes of parts
+
     return {
         "trainer": trainer * TRAINER_SIZE,
         "prg": prgBanks * PRG_BANK_SIZE,
@@ -117,6 +98,7 @@ def parse_iNES_header(header, file):
 
 def get_iNES_info(handle):
     """Get and validate sizes of iNES file parts from header."""
+
     size = handle.seek(0, 2)
     if size < HEADER_SIZE:
         exit("Error: the file is smaller than the iNES header size.")
@@ -129,20 +111,17 @@ def get_iNES_info(handle):
     if size != correctSize:
         msg = (
             "Error: the file is {:d} bytes; according to the header, it "
-            "should be {:d} bytes (= header {:d} + trainer {:d} + PRG-ROM "
-            "{:d} + CHR-ROM {:d} bytes)"
-        ).format(
-            size, correctSize, HEADER_SIZE, info["trainer"], info["prg"],
-            info["chr"]
-        )
+            "should be {:d} bytes."
+        ).format(size, correctSize)
         exit(msg)
     return info
 
 def read_file(handle, pos, bytesLeft):
     """Yield a slice from a file in chunks."""
+
     while bytesLeft > 0:
         chunkSize = min(bytesLeft, FILE_BUFFER_MAX_SIZE)
-        handle.seek(pos)  # must be set before each yield
+        handle.seek(pos)
         yield handle.read(chunkSize)
         pos += chunkSize
         bytesLeft -= chunkSize
@@ -150,11 +129,12 @@ def read_file(handle, pos, bytesLeft):
 def is_file_part_splittable(handle, start, length):
     """Can the PRG-ROM or CHR-ROM data be split in half? That is, are the
     halves identical, a power of two and large enough?"""
-    # length must be 2**5 (two CHR tiles) to 2**21 (maximum PRG size)
-    if length not in (2**exp for exp in range(5, 21+1)):
+
+    # length must be at least 2 and a power of two
+    if length < 2 or length & (length - 1):  # see references
         return False
-    halfLength = length // 2
     # compare halves chunk by chunk
+    halfLength = length // 2
     return all(
         chunk1 == chunk2 for (chunk1, chunk2) in zip(
             read_file(handle, start, halfLength),
@@ -165,6 +145,7 @@ def is_file_part_splittable(handle, start, length):
 def copy_slice(source, bytesLeft, target):
     """Copy a slice from current position in source file to start of target
     file in chunks."""
+
     target.seek(0)
     while bytesLeft > 0:
         chunkSize = min(bytesLeft, FILE_BUFFER_MAX_SIZE)
@@ -173,11 +154,12 @@ def copy_slice(source, bytesLeft, target):
 
 def copy_parts(source, settings):
     """Copy parts of input file to output files."""
+
     info = get_iNES_info(source)
     # Trainer
     if settings["trainer"] is not None:
         if info["trainer"] == 0:
-            print("Warning: no Trainer: {:s}".format(to_ASCII(source.name)))
+            print("Warning: no Trainer.")
         else:
             source.seek(HEADER_SIZE)
             with open(settings["trainer"], "wb") as target:
@@ -185,11 +167,11 @@ def copy_parts(source, settings):
     # PRG-ROM
     if settings["prg"] is not None:
         if info["prg"] == 0:
-            print("Warning: no PRG-ROM: {:s}".format(to_ASCII(source.name)))
+            print("Warning: no PRG-ROM.")
         else:
             start = HEADER_SIZE + info["trainer"]
             length = info["prg"]
-            # split in half as many times as possible
+            # split as many times as possible
             while is_file_part_splittable(source, start, length):
                 length //= 2
             # copy
@@ -199,11 +181,11 @@ def copy_parts(source, settings):
     # CHR-ROM
     if settings["chr"] is not None:
         if info["chr"] == 0:
-            print("Warning: no CHR-ROM: {:s}".format(to_ASCII(source.name)))
+            print("Warning: no CHR-ROM.")
         else:
             start = HEADER_SIZE + info["trainer"] + info["prg"]
             length = info["chr"]
-            # split in half as many times as possible
+            # split as many times as possible
             while is_file_part_splittable(source, start, length):
                 length //= 2
             # copy
@@ -212,12 +194,7 @@ def copy_parts(source, settings):
                 copy_slice(source, length, target)
 
 def main():
-    # parse arguments
-    if len(sys.argv) == 1:
-        print(HELP_TEXT)
-        exit(0)
     settings = parse_arguments()
-    # copy parts of input file to output files
     try:
         with open(settings["source"], "rb") as source:
             copy_parts(source, settings)
